551df3f44850986ed4618900abeacf3920646a6b
anna <bogdanovich.av812@gmail.com>
2023-06-01 22:09:41 +0300

    add new func

--- homework.js
+++ homework.js
@@ -1,11 +1,11 @@
 function clickMe() {
 
     let username = document.getElementById("username");
-    console.log(username);
+
     let a = username.value.trim(username);
-    console.log(a);
+
     let usname = a.charAt(0).toUpperCase() + a.slice(1).toLowerCase();
-    console.log(usname);
+
     let chat = document.getElementById("chat");
     chat.innerHTML = usname;
 
@@ -15,28 +15,51 @@ function clickMe() {
     let cleanComment = document.querySelector("#clean-comment");
     cleanComment.innerHTML = clean;
 
+    let imgSrc = document.getElementById("avatar");
+    document.querySelector("#avatar-img").src = imgSrc.value;
 
+
+
+    //добавление текущей даты
     let date = document.querySelector('#date');
-
     let today = new Date();
     let now = today.toLocaleString();
     date.innerHTML = now;
 
-
+    //удаление поля имени
     let yes = document.querySelector('#yes');
-
     yes.addEventListener('change', () => {
-        if (yes.hasAttribute('checked') == false) {
+        console.log(yes);
+        if (yes.hasAttribute(checked) == false) {
+
             let usernameInput = document.getElementById('username-input');
             usernameInput.setAttribute('style', 'display: none;');
         }
     }
-
     )
 
+    //подстановка имени пользователя
+    username.addEventListener('change', () => {
+        if (username.value == "") {
+            chat.value.innerHTML = 'Name';
+            console.log(chat);
+        }
+    }
+    )
+
+    //замена аватара
+    /*comments.addEventListener('change', () => {
+        if (comments.value != "") {
+            avatar.src = imgSrc.value;
+
+        }
+        else {
 
 
-    let imgSrc = document.getElementById("avatar");
-    document.querySelector("#avatar-img").src = imgSrc.value;
+
+        }
+    }
+
+    )*/
 
 }
